ea4a5a40c46797a59f228491d87c6b5484eb99c4
dgoldsb <dgoldsb@live.nl>
2019-07-12 12:20:09 +0200

    Few new aliases and new plugin

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -16,9 +16,47 @@ alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcaa='git commit -a --amend -C HEAD'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+# Make an AAA ticket branch.
+branchaaa() {
+    eval "git pull && git checkout -b dgoldsborough/AAA-$1"
+}
+
+# Make a DATA ticket branch.
+branchdata() {
+    eval "git pull && git checkout -b dgoldsborough/DATA-$1"
+}
+
+# Commit easily to an AAA ticket branch.
+function gitaaa() {
+    if [[ "$(git rev-parse --abbrev-ref HEAD)" =~ 'AAA-[0-9]*' ]]; then
+        ticket=$MATCH
+  	    echo "Writing a commit with ticket ${ticket}"
+  	    command="git commit -m \"$ticket $1\""
+  	    eval "${command}"
+    else
+        echo "Not an AAA ticket branch!"
+    fi
+}
+
+# Commit easily to a DATA ticket branch.
+function gitdata() {
+    if [[ "$(git rev-parse --abbrev-ref HEAD)" =~ "DATA-[0-9]*" ]]; then
+        ticket=$MATCH
+  	    echo "Writing a commit with ticket ${ticket}"
+  	    command="git commit -m \"$ticket $1\""
+  	    eval "${command}"
+    else
+        echo "Not a DATA ticket branch!"
+    fi
+}
+
+# Delete local branches
+alias gclean='git branch -D `git branch | grep "DATA\|AAA\|PDP" | xargs`'
